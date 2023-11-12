dae195e4ea8c98d39c23d8a31edfeaf8cb7a315d
paul <pmolch3.1415@gmail.com>
2023-11-12 22:03:39 +0300

    init fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import "./font-awesome.js"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'City Park';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +17,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
---
+++ resources/js/font-awesome.js
@@ -0,0 +1,70 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import {
+    faCircleCheck, faR,
+    faS,
+    faUserSecret,
+} from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+/*
+Шапка
+<font-awesome-icon :icon="['fas', 'house']" />
+<font-awesome-icon :icon="['fas', 'utensils']" />
+<font-awesome-icon :icon="['fas', 'hotel']" />
+<font-awesome-icon :icon="['fas', 'address-book']" />
+<font-awesome-icon :icon="['fas', 'right-to-bracket']" />
+Доставка еды
+<font-awesome-icon :icon="['fas', 'truck']" />
+Администрирование
+<font-awesome-icon :icon="['fas', 'wand-magic-sparkles']" />
+Карточки
+<font-awesome-icon :icon="['fas', 'cart-plus']" />
+<font-awesome-icon :icon="['fas', 'phone']" />
+<font-awesome-icon :icon="['fas', 'eye']" />
+Контактная информация
+<font-awesome-icon :icon="['fas', 'location-dot']" />
+<font-awesome-icon :icon="['fas', 'phone']" />
+<font-awesome-icon :icon="['fas', 'face-grin-wink']" />
+Соц. сети
+<font-awesome-icon :icon="['fab', 'vk']" />
+<font-awesome-icon :icon="['fab', 'square-odnoklassniki']" />
+<font-awesome-icon :icon="['fab', 'square-instagram']" />
+3D панорама
+<font-awesome-icon :icon="['fas', 'vr-cardboard']" />
+Дата события
+<font-awesome-icon icon="calendar-days" />
+Стрелки
+<font-awesome-icon :icon="['fas', 'arrow-down']" />
+<font-awesome-icon :icon="['fas', 'arrow-right']" />
+Блог повара
+<font-awesome-icon :icon="['fas', 'glass-water']" />
+<font-awesome-icon :icon="['fas', 'square']" />
+<font-awesome-icon :icon="['far', 'square']" />
+<font-awesome-icon :icon="['fas', 'pizza-slice']" />
+Каталог номеров
+<font-awesome-icon :icon="['fas', 'fan']" />
+<font-awesome-icon :icon="['fas', 'tablet']" />
+<font-awesome-icon :icon="['fas', 'box-archive']" />
+<font-awesome-icon :icon="['fas', 'phone-volume']" />
+<font-awesome-icon :icon="['fas', 'tv']" />
+<font-awesome-icon :icon="['fas', 'couch']" />
+<font-awesome-icon :icon="['far', 'snowflake']" />
+<font-awesome-icon :icon="['fas', 'toilet-portable']" />
+Формы
+<font-awesome-icon :icon="['fas', 'paper-plane']" />
+<font-awesome-icon :icon="['fas', 'eye']" />
+<font-awesome-icon :icon="['fas', 'eye-slash']" />
+<font-awesome-icon :icon="['fas', 'plus']" />
+<font-awesome-icon :icon="['fas', 'minus']" />
+<font-awesome-icon :icon="['fas', 'trash-can']" />
+<font-awesome-icon :icon="['fas', 'circle-xmark']" />
+<font-awesome-icon :icon="['fas', 'circle-check']" />
+<font-awesome-icon :icon="['far', 'circle-dot']" />
+<font-awesome-icon :icon="['fas', 'circle-dot']" />
+<font-awesome-icon :icon="['fas', 'clock']" />
+<font-awesome-icon :icon="['fas', 'square-check']" />
+<font-awesome-icon :icon="['fas', 'sort-down']" />
+*/
+
+library.add(faS, faR, far, faCircleCheck, faUserSecret)
