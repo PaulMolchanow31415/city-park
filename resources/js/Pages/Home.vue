645ffa21ffb35d20dfbf8c906e902d0410f7afa3
Paul Molchanov <pmolch3.1415@gmail.com>
2023-11-16 11:27:07 +0300

    add header & swiper

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,22 +1,53 @@
 <script setup>
-import AppLogo from '@/Components/AppLogo.vue';
+import AppLogo from "@/Components/AppLogo.vue";
 </script>
 
 <template>
-    <Head title="Главная"/>
+    <Head title="Главная" />
     <header class="px-1 md:px-3">
         <nav class="container mx-auto flex items-center justify-between gap-4">
-            <AppLogo class="w-20 pb-5"/>
+            <AppLogo class="w-20 pb-5" />
             <ul class="flex gap-5">
                 <li>
                     <Link class="link" :href="route('home')">
-                        <font-awesome-icon size="xs" :icon="['fas', 'house']"/>
+                        <font-awesome-icon size="xs" :icon="['fas', 'house']" />
                         Главная
+                    </Link>
+                </li>
+                <li>
+                    <Link class="link" :href="route('welcome')">
+                        <font-awesome-icon
+                            size="xs"
+                            :icon="['fas', 'utensils']"
+                        />
+                        Ресторан и летнее кафе
+                    </Link>
+                </li>
+                <li>
+                    <Link class="link" :href="route('dashboard')">
+                        <font-awesome-icon size="xs" :icon="['fas', 'hotel']" />
+                        Отель-люкс
+                    </Link>
+                </li>
+                <li>
+                    <Link class="link" :href="route('home')">
+                        <font-awesome-icon :icon="['fas', 'address-book']" />
+                        Контакты
+                    </Link>
+                </li>
+                <li>
+                    <Link class="link" :href="route('login')">
+                        <font-awesome-icon
+                            size="xs"
+                            :icon="['fas', 'right-to-bracket']"
+                        />
+                        Войти
                     </Link>
                 </li>
             </ul>
         </nav>
     </header>
+    <div class="slider"></div>
     <h1 class="text-center mt-32 text-xl">Вкусные щи</h1>
 </template>
 
@@ -24,9 +55,14 @@ import AppLogo from '@/Components/AppLogo.vue';
 .link {
     @apply flex gap-1.5 px-5 py-3 relative items-center;
     &::after {
-        @apply absolute block bottom-0 left-0 w-1/6;
-        content: '';
-        height: 1px;
+        @apply absolute block bottom-3 left-5 w-0 h-px content-[''] opacity-0 transition-all ease-in-out;
+        background: var(--secondary-dark-color);
+    }
+    &:hover {
+        color: var(--secondary-dark-color);
+        &::after {
+            @apply w-10 opacity-100;
+        }
     }
 }
 </style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,113 +1,58 @@
-<script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { reactive } from 'vue';
-
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-});
-
-const state = reactive({ isBounce: false})
-function bounce() {
-    setInterval(() => state.isBounce = !state.isBounce, 2000)
-}
-
-requestAnimationFrame(bounce)
-</script>
-
 <template>
-    <Head title="Welcome"/>
-
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div v-if="canLogin" class="heading">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-                  class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                Dashboard
-            </Link>
-
-            <template v-else>
-                <Link :href="route('login')"
-                      class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                    Log in
-                </Link>
-
-                <Link v-if="canRegister" :href="route('register')"
-                      class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                    Register
-                </Link>
-            </template>
-        </div>
-
-
-        <!-- Modal toggle -->
-        <button data-modal-target="default-modal" data-modal-toggle="default-modal"
-                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button">
-            <FontAwesomeIcon :bounce="state.isBounce" :icon="['fas', 'circle-check']" />
-            Toggle modal
-            <FontAwesomeIcon :icon="['fas', 'fa-user-secret']" />
-        </button>
-
-        <!-- Main modal -->
-        <div id="default-modal" tabindex="-1" aria-hidden="true"
-             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-2xl max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                            Terms of Service
-                        </h3>
-                        <button type="button"
-                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                data-modal-hide="default-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-4 md:p-5 space-y-4">
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for
-                            its citizens, companies around the world are updating their terms of service agreements to
-                            comply.
-                        </p>
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May
-                            25 and is meant to ensure a common set of data rights in the European Union. It requires
-                            organizations to notify users as soon as possible of high-risk data breaches that could
-                            personally affect them.
-                        </p>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="default-modal" type="button"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            I accept
-                        </button>
-                        <button data-modal-hide="default-modal" type="button"
-                                class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                            Decline
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-    </div>
+    <swiper
+        :effect="'coverflow'"
+        :grabCursor="true"
+        :centeredSlides="true"
+        :slidesPerView="'auto'"
+        :coverflowEffect="{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        }"
+        :pagination="true"
+        :modules="modules"
+        class="mySwiper"
+    >
+        <swiper-slide
+            ><img
+                src="https://plus.unsplash.com/premium_photo-1699600552748-3bb02605fc4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+                alt=""
+        /></swiper-slide>
+        <swiper-slide
+            ><img
+                src="https://plus.unsplash.com/premium_photo-1699600552748-3bb02605fc4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+                alt=""
+        /></swiper-slide>
+        <swiper-slide
+            ><img
+                src="https://plus.unsplash.com/premium_photo-1699600552748-3bb02605fc4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+                alt=""
+        /></swiper-slide>
+    </swiper>
 </template>
+<script>
+// import Swiper core and required modules
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/vue";
 
-<style scoped lang="scss">
-.heading {
-    @apply sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10;
-}
-</style>
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+// Import Swiper styles
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    data() {
+        return {
+            modules: [EffectCoverflow, Pagination],
+        };
+    },
+};
+</script>
